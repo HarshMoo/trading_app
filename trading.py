45301cd5400cb69998395f8b377b7a1aaf7c4fa5
Harsh Moon <shitluna50kgone@gmail.com>
2024-02-08 20:56:26 +0530

    second commit

--- trading.py
+++ trading.py
@@ -18,21 +18,6 @@ def get_price(ticker_symbol):
 def get_volume(ticker_symbol):
 
     return get_data(ticker_symbol,"Volume")
-
-def get_pe_ratio(ticker_symbol):
-    try:
-        ticker = yf.Ticker(ticker_symbol)
-        
-        current_price = ticker.history(period='1d')['Close'][0]
-        
-        earnings_per_share = ticker.earnings['Earnings'].iloc[-1]  # Get the latest reported earnings
-        
-        pe_ratio = current_price / earnings_per_share
-        
-        return pe_ratio
-    except Exception as e:
-        print(f"Error fetching data: {e}")
-        return None
     
 def get_dividend_yield(ticker_symbol):
     try:
